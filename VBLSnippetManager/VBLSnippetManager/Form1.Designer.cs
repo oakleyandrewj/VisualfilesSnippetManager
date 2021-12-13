89482279179277d72ef0094be699ee9cb54c9997
oakleyandrewj <oakleyandrewj@gmail.com>
2021-12-13 16:28:01 +0000

    Add files via upload

---
+++ VBLSnippetManager/VBLSnippetManager/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace VBLSnippetManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel_Main = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxSnippets = new System.Windows.Forms.ListBox();
+            this.btnAddSnippet = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtEditor = new System.Windows.Forms.RichTextBox();
+            this.btnCopyClipboard = new System.Windows.Forms.Button();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel_Main.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel_Main
+            // 
+            this.tableLayoutPanel_Main.ColumnCount = 4;
+            this.tableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.29889F));
+            this.tableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 84.70111F));
+            this.tableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+            this.tableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel_Main.Controls.Add(this.listBoxSnippets, 0, 2);
+            this.tableLayoutPanel_Main.Controls.Add(this.btnAddSnippet, 0, 3);
+            this.tableLayoutPanel_Main.Controls.Add(this.tableLayoutPanel1, 1, 2);
+            this.tableLayoutPanel_Main.Controls.Add(this.btnCopyClipboard, 2, 2);
+            this.tableLayoutPanel_Main.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel_Main.Controls.Add(this.txtFilter, 0, 1);
+            this.tableLayoutPanel_Main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel_Main.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel_Main.Name = "tableLayoutPanel_Main";
+            this.tableLayoutPanel_Main.RowCount = 4;
+            this.tableLayoutPanel_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.89933F));
+            this.tableLayoutPanel_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.10067F));
+            this.tableLayoutPanel_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 347F));
+            this.tableLayoutPanel_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+            this.tableLayoutPanel_Main.Size = new System.Drawing.Size(989, 446);
+            this.tableLayoutPanel_Main.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Available Snippets";
+            // 
+            // listBoxSnippets
+            // 
+            this.listBoxSnippets.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxSnippets.FormattingEnabled = true;
+            this.listBoxSnippets.Location = new System.Drawing.Point(3, 52);
+            this.listBoxSnippets.Name = "listBoxSnippets";
+            this.listBoxSnippets.Size = new System.Drawing.Size(131, 341);
+            this.listBoxSnippets.TabIndex = 1;
+            this.listBoxSnippets.SelectedIndexChanged += new System.EventHandler(this.listBoxSnippets_SelectedIndexChanged);
+            // 
+            // btnAddSnippet
+            // 
+            this.btnAddSnippet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAddSnippet.Location = new System.Drawing.Point(3, 399);
+            this.btnAddSnippet.Name = "btnAddSnippet";
+            this.btnAddSnippet.Size = new System.Drawing.Size(131, 44);
+            this.btnAddSnippet.TabIndex = 2;
+            this.btnAddSnippet.Text = "Add Snippet";
+            this.btnAddSnippet.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtEditor, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(140, 52);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.82621F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.17379F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(758, 341);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.txtName, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.txtDescription, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(752, 30);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Snippet Name";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(224, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Snippet Description";
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtName.Location = new System.Drawing.Point(83, 5);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(135, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtDescription.Location = new System.Drawing.Point(329, 3);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(420, 20);
+            this.txtDescription.TabIndex = 3;
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtEditor.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditor.Location = new System.Drawing.Point(3, 39);
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.Size = new System.Drawing.Size(752, 299);
+            this.txtEditor.TabIndex = 1;
+            this.txtEditor.Text = "";
+            this.txtEditor.WordWrap = false;
+            // 
+            // btnCopyClipboard
+            // 
+            this.btnCopyClipboard.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnCopyClipboard.Location = new System.Drawing.Point(904, 331);
+            this.btnCopyClipboard.Name = "btnCopyClipboard";
+            this.btnCopyClipboard.Size = new System.Drawing.Size(61, 62);
+            this.btnCopyClipboard.TabIndex = 4;
+            this.btnCopyClipboard.Text = "Copy To Clipboard";
+            this.btnCopyClipboard.UseVisualStyleBackColor = true;
+            this.btnCopyClipboard.Click += new System.EventHandler(this.btnCopyClipboard_Click);
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtFilter.Location = new System.Drawing.Point(3, 20);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(131, 20);
+            this.txtFilter.TabIndex = 5;
+            this.txtFilter.Text = "Filter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 446);
+            this.Controls.Add(this.tableLayoutPanel_Main);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "VBLSnippetManager";
+            this.tableLayoutPanel_Main.ResumeLayout(false);
+            this.tableLayoutPanel_Main.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_Main;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxSnippets;
+        private System.Windows.Forms.Button btnAddSnippet;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.RichTextBox txtEditor;
+        private System.Windows.Forms.Button btnCopyClipboard;
+        private System.Windows.Forms.TextBox txtFilter;
+    }
+}
+
---
+++ VBLSnippetManager/VBLSnippetManager/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace VBLSnippetManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSnippetsFromXmlFile();
+        }
+
+        private void btnCopyClipboard_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(txtEditor.Text);
+        }
+
+        private void LoadSnippetsFromXmlFile()
+        {
+            XmlDocument xml = new XmlDocument();
+            var contents = "";
+            using (StreamReader streamReader = new StreamReader("C:\\Users\\Oakley\\Source\\repos\\VBLSnippetManager\\VBLSnippetManager\\snippets.xml", Encoding.UTF8))
+            {
+                contents = streamReader.ReadToEnd();
+            }
+            xml.LoadXml(contents);
+
+            XmlNodeList xnList = xml.SelectNodes("/snippets/snippet");
+            foreach (XmlNode xn in xnList)
+            {
+                string title = xn["title"].InnerText;
+                //string lastName = xn["description"].InnerText;
+                //txtEditor.Text += firstName + lastName;
+                listBoxSnippets.Items.Add(title);
+            }
+
+        }
+
+        private void listBoxSnippets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //System.Windows.Forms.MessageBox.Show(listBoxSnippets.SelectedItem.ToString());
+            string title = "";
+            string description = "";
+            string content = "";
+
+            XmlDocument xml = new XmlDocument();
+            var contents = "";
+            using (StreamReader streamReader = new StreamReader("C:\\Users\\Oakley\\Source\\repos\\VBLSnippetManager\\VBLSnippetManager\\snippets.xml", Encoding.UTF8))
+            {
+                contents = streamReader.ReadToEnd();
+            }
+            xml.LoadXml(contents);
+
+            XmlNodeList xnList = xml.SelectNodes("/snippets/snippet");
+            foreach (XmlNode xn in xnList)
+            {
+                if (listBoxSnippets.SelectedItem.ToString() == xn["title"].InnerText)
+                {
+                    title = xn["title"].InnerText;
+                    description = xn["description"].InnerText;
+                    content = xn["content"].InnerText;
+
+                }
+                txtName.Text = title;
+                txtDescription.Text = description;
+                txtEditor.Text = content.Replace("*","&");
+                
+            }
+        }
+    }
+}
