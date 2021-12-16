dbd2786318a62f95668a7b6ae657c3531c6c0e9c
Andrew Oakley <oakleyandrewj@gmail.com>
2021-12-16 11:22:14 +0000

    code formatting

--- VBLSnippetManager/VBLSnippetManager/Form1.cs
+++ VBLSnippetManager/VBLSnippetManager/Form1.cs
@@ -44,8 +44,6 @@ namespace VBLSnippetManager
             foreach (XmlNode xn in xnList)
             {
                 string title = xn["title"].InnerText;
-                //string lastName = xn["description"].InnerText;
-                //txtEditor.Text += firstName + lastName;
                 listBoxSnippets.Items.Add(title);
             }
             _itemList = listBoxSnippets.Items.Cast<string>().ToList();
@@ -53,7 +51,6 @@ namespace VBLSnippetManager
 
         private void listBoxSnippets_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //System.Windows.Forms.MessageBox.Show(listBoxSnippets.SelectedItem.ToString());
             string title = "";
             string description = "";
             string content = "";
@@ -74,35 +71,25 @@ namespace VBLSnippetManager
                     title = xn["title"].InnerText;
                     description = xn["description"].InnerText;
                     content = xn["content"].InnerText;
-
                 }
                 txtName.Text = title;
                 txtDescription.Text = description;
                 txtEditor.Text = content.Replace("*","&");
-                
             }
         }
 
         private void btnAddSnippet_Click(object sender, EventArgs e)
         {
-
-            //call new form and passback values
             Form_New entryForm = new Form_New();
-            //clear form content
             entryForm.ShowDialog();
-
             LoadSnippetsFromXmlFile();
         }
 
         private void txtFilter_TextChanged(object sender, EventArgs e)
         {
-            //var itemList = listBoxSnippets.Items.Cast<string>().ToList();
             if (_itemList.Count > 0)
             {
-                //clear the items from the list
                 listBoxSnippets.Items.Clear();
-
-                //filter the items and add them to the list
                 listBoxSnippets.Items.AddRange(
                     _itemList.Where(i => i.Contains(txtFilter.Text)).ToArray());
             }
